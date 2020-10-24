359ca7921d0d02d48ab46b4977ea988e83326a91
Meir Gabay <meir@prodops.io>
2020-10-24 21:13:42 +0300

    add tests

---
+++ tests/test_api.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+from appy.utils import message
+
+
+class ApiTestCase(TestCase):
+
+    def test_get_fact(self):
+        fact = message.get_fact()
+        self.assertTrue(isinstance(fact, str) and len(fact) > 2)
---
+++ tests/test_greet.py
@@ -0,0 +1,30 @@
+from unittest import TestCase, mock
+from io import StringIO
+from appy.core import app
+
+
+class GreetTestCase(TestCase):
+
+    @mock.patch('sys.stdout', new_callable=StringIO)
+    def test_fullname(self, mock_stdout):
+        with mock.patch('builtins.input', return_value="meir gabay"):
+            app.main()
+            self.assertTrue(
+                'Hello Meir Gabay' in mock_stdout.getvalue()
+            )
+
+    @mock.patch('sys.stdout', new_callable=StringIO)
+    def test_firstname(self, mock_stdout):
+        with mock.patch('builtins.input', return_value="meir"):
+            app.main()
+            self.assertTrue(
+                'Hello Meir' in mock_stdout.getvalue()
+            )
+
+    @mock.patch('sys.stdout', new_callable=StringIO)
+    def test_number(self, mock_stdout):
+        with mock.patch('builtins.input', return_value="123"):
+            app.main()
+            self.assertTrue(
+                'Hello 123' in mock_stdout.getvalue()
+            )
