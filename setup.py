16b04bcb8e7a3a9c56576f3a16f24db60efe0869
Meir Gabay <unfor19@gmail.com>
2020-10-31 23:42:16 +0200

    migrate from setup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-from setuptools import setup, find_packages  # noqa: 401
+from os import getcwd, path
+from setuptools import setup
 
 with open("README.md", "r") as fh:
     readme = fh.read()
@@ -6,42 +7,10 @@ with open("README.md", "r") as fh:
 with open("version", "r") as fh:
     version = fh.read()
 
-with open("appy/__init__.py", "w") as fh:
+with open(path.join(getcwd(), 'appy', '__init__.py'), "w") as fh:
     fh.write(f"__version__ = '{version}'")
 
 setup(
-    author='Meir Gabay',
-    author_email='unfor19@gmail.com',
-    name='unfor19-appy',
-    license='MIT',
-    long_description_content_type='text/markdown',
-    description='Python project template',  # noqa: E501
-    long_description=readme,
     version=f'{version}',
-    # packages=find_packages() # must add __init__.py to each package directory
-    packages=['appy', 'appy.core', 'appy.utils'],
-    keywords='python project template',
-    include_package_data=True,
-    url='https://github.com/unfor19/python-project',
     download_url=f'https://github.com/unfor19/python-project/archive/v{version}.tar.gz',  # noqa: E501
-    entry_points='''
-        [console_scripts]
-        appy=appy.__main__:main
-    ''',
-    setup_requires=[
-        'setuptools>=44.1.0',
-        'wheel>=0.34.2'
-    ],
-    install_requires=[
-        'requests>=2.24.0'
-    ],
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8'
-    ],
 )
