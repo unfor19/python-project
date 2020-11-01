2f18794a16a65499408f1f9a0cf50cf85c53b5ff
Meir Gabay <unfor19@gmail.com>
2020-11-01 23:45:13 +0200

    treat version as is

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from os import getcwd, path
-from setuptools import setup
+from setuptools import setup, sic
 
 with open("README.md", "r") as fh:
     readme = fh.read()
@@ -8,9 +8,9 @@ with open("version", "r") as fh:
     version = fh.read()
 
 with open(path.join(getcwd(), 'src', 'appy', '__init__.py'), "w") as fh:
-    fh.write(f"__version__ = '{version}'")
+    fh.write(f"__version__ = '{version}'\n")
 
 setup(
-    version=f'{version}',
+    version=sic(version),
     download_url=f'https://github.com/unfor19/python-project/archive/{version}.tar.gz',  # noqa: E501
 )
