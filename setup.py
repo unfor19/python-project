66fadf4c47331150fc08b5de279d14b684e3f289
Meir Gabay <unfor19@gmail.com>
2020-11-02 01:00:03 +0200

    avoid embedding values

--- setup.py
+++ setup.py
@@ -1,16 +1,25 @@
 from os import getcwd, path
 from setuptools import setup
 
+# Change accordingly ----------------------------
+PACKAGE_NAME = "appy"
+GITHUB_OWNER = "unfor19"
+GITHUB_REPOSITORY = "python-project"
+# -----------------------------------------------
+
+
+# Keep the same structure, should NOT be changed
 with open("README.md", "r") as fh:
     readme = fh.read()
 
 with open("version", "r") as fh:
     version = fh.read()
 
-with open(path.join(getcwd(), 'src', 'appy', '__init__.py'), "w") as fh:
+with open(path.join(getcwd(), 'src', PACKAGE_NAME, '__init__.py'), "w") as fh:
     fh.write(f"__version__ = '{version}'\n")
 
 setup(
     version=version,
-    download_url=f'https://github.com/unfor19/python-project/archive/{version}.tar.gz',  # noqa: E501
+    download_url=f'https://github.com/{GITHUB_OWNER}/{GITHUB_REPOSITORY}/archive/{version}.tar.gz',  # noqa: E501
 )
+# -----------------------------------------------
