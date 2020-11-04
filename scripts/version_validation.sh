672f4fc22cd3313c44160a51abf334b05a26b8dc
Meir Gabay <meir@prodops.io>
2020-11-04 20:20:47 +0200

    using perl to support positive lookbehind

--- scripts/version_validation.sh
+++ scripts/version_validation.sh
@@ -17,7 +17,8 @@ msg_log(){
     fi
 }
 
-if [[ $_RELEASE_VERSION =~ ^[0-9]{1,}(\.[0-9]*)*(\.[0-9]{1,}(a|b|rc)[0-9]{1,}|(\.post[0-9]{1,})|(\.dev[0-9]{1,})){0,1}(?<=[0-9]){1,}$ ]]; then
+_CHECK_RELEASE_VERSION=$(echo "$_RELEASE_VERSION" | perl -ne 'print $1 if /^[0-9]{1,}(\.[0-9]*)*(\.[0-9]{1,}(a|b|rc)[0-9]{1,}|(\.post[0-9]{1,})|(\.dev[0-9]{1,})){0,1}(?<=[0-9]){1,}$/s')
+if [[ -n $_CHECK_RELEASE_VERSION ]]; then
     msg_log "Passed - Release version is valid - $_RELEASE_VERSION"
     echo "$_RELEASE_VERSION"
 else
