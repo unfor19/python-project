127bf275e943ae9621cf5a8cb1e73d031d480f14
Meir Gabay <meir@prodops.io>
2020-11-04 19:43:32 +0200

    update ref to src

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import runpy
 
 
 def main():
-    appy_package = runpy.run_module(mod_name="appy", init_globals=globals())
+    appy_package = runpy.run_module(
+        mod_name="src.appy", init_globals=globals())
     appy_package['message'].script_path(__file__)
     appy_package['main']()
 
